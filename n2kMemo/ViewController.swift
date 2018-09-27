4c62161b517fdc9c10160e911bfc8c264a4205eb
Mark Lucking <mark.lucking@gmail.com>
2018-09-27 09:37:44 +0200

    Initial Commit

---
+++ n2kMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  n2kMemo
+//
+//  Created by localadmin on 27.09.18.
+//  Copyright © 2018 ch.cqd.n2kMemo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
